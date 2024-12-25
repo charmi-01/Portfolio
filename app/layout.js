b5ff958b2bd38749beed068322c44f5ac4fc53c3
chirag <chiragsharma1676@gmail.com>
2024-12-25 12:44:06 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,11 @@ import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
-import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
+
+const ScrollToTopDynamic = dynamic(() => import("./components/helper/scroll-to-top"), {
+  ssr: false,
+});
 
 export const metadata = {
   title: "Chirag Sharma - Software Developer",
@@ -22,7 +25,7 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
-          <ScrollToTop />
+          <ScrollToTopDynamic />
         </main>
         <Footer />
       </body>
