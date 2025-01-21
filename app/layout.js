867e094479bfd1303e287b1de90a2b5618760072
chirag <chiragsharma1676@gmail.com>
2025-01-22 00:02:37 +0530

    build fixes

--- app/layout.js
+++ app/layout.js
@@ -5,13 +5,12 @@ import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
-import ScrollToTopDynamic from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Chirag Sharma - Software Developer",
   description:
-    "This is the portfolio of Chirag Sharma. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  "This is the portfolio of Chirag Sharma. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
@@ -22,7 +21,6 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
-          <ScrollToTopDynamic />
         </main>
         <Footer />
       </body>
