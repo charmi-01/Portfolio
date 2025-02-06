843d4d1afb9a8c11517ac435dafbf501b30f2e10
chirag <chiragsharma1676@gmail.com>
2025-02-07 00:04:44 +0530

    updated github

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
     email: 'chiragsharma1676@gmail.com',
     phone: '+918924803308',
     address: 'Gurgaon, Haryana, India',
-    github: 'https://github.com/charmi-01',
+    github: 'https://github.com/chirag-0110',
     linkedIn: 'https://www.linkedin.com/in/chirag-sharma-04a6b4202/',
     devUsername: "chirag0110",
 }
